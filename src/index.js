596ef680961779a113202830b3b2d9c7db8456f5
manikantkg <manikantacoder007@gmail.com>
2024-08-14 15:05:33 +0530

    styling component and boostrap added

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppClass from './AppClass';
+import HelloWorld from './HelloWorld';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className='container'>
+      <div className='row'>
+        <div className='column'>
+          <AppClass />
+          <HelloWorld />
+        </div>
+      </div>
+    </div>
+  </React.StrictMode>
+);
