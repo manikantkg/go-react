51d0e4e90f5123214fdff2eb73586c7918fcd65e
manikantkg <manikantacoder007@gmail.com>
2024-08-21 19:09:25 +0530

    react and state1

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,11 +1,33 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useState } from "react";
 import './HelloWorld.css';
 
-function HelloWorld(props){
-    return(
+function HelloWorld(props) {
+    const [isTrue, setIsTrue] = useState(true)
+    const toggleTrue = () => {
+        if (isTrue){
+            setIsTrue(false)
+            return
+        }
+        setIsTrue(true)
+    }
+    return (
         <Fragment>
-        <hr />
-        <h1 className="h1-blue">{props.msg}</h1>
+            <hr />
+            <h1 className="h1-blue">{props.msg}</h1>
+            <hr />
+            {isTrue &&
+
+                <Fragment>
+                    <p>The current value is True</p>
+                </Fragment>
+            }
+            <hr />
+            {isTrue
+                ? <p>isTrue</p>
+                :<p>isFalse</p>
+            }
+            <hr />
+            <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle is true</a>
         </Fragment>
     )
 }
