88342605d21d8a9523ba8c2e0762abc10f6d1552
manikantkg <manikantacoder007@gmail.com>
2024-09-23 18:40:33 +0530

    state used in appclass

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,10 +1,49 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import './AppClass.css';
 
 export default class AppClass extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            isTrue: false,
+        };
+        return 
+    }
+
+    toggleTrue = () =>{
+        if (this.state.isTrue){
+            this.setState({
+                isTrue: false,
+            });
+            return
+        }
+        this.setState({
+            isTrue: true,
+        
+        })
+    }
     render(){
         return(
-            <h1 className="h1-red">{this.props.msg}</h1>
+           <>
+             <hr />
+            <h1 className="h1-blue">{this.props.msg}</h1>
+            <hr />
+            {this.state.isTrue &&
+
+                <Fragment>
+                    <p>The current value is True</p>
+                </Fragment>
+            }
+            <hr />
+            {this.state.isTrue
+                ? <p>isTrue</p>
+                :<p>isFalse</p>
+            }
+            <hr />
+            <a href="#!" className="btn btn-outline-secondary" onClick={this.toggleTrue}>Toggle is true</a>
+           
+           
+           </>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloWorld from './HelloWorld';
+import AppClass from './AppClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='container'>
-      <div className='row'>
+      <div className='row'> 
         <div className='column'>
-          <HelloWorld msg='World'/>
+          <AppClass msg='state using World'/>
+         
         </div>
       </div>
     </div>
